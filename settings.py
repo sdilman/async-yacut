4a644da65c0ab1ed5316ffdadfc58fc1bd964a8c
Stepan Dilman <stepandilman@Mac.Dlink>
2025-07-13 17:21:40 +0300

    Adjustment for tests

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
-    SECRET_KEY = os.environ['SECRET_KEY']
-    DISK_TOKEN = os.environ['DISK_TOKEN']
-    API_HOST = os.environ['API_HOST']
-    API_VERSION = os.environ['API_VERSION']
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DISK_TOKEN = os.getenv('DISK_TOKEN')
+    API_HOST = os.getenv('API_HOST')
+    API_VERSION = os.getenv('API_VERSION')
